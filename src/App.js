995c1eaa844c948c1801db92a889c8d1d6eabddc
Aniket06062002 <75536999+Aniket06062002@users.noreply.github.com>
2023-09-04 13:35:36 +0530

    All Project file added

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from './Components/Header';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import './Styles/style.css';
+// import './Styles/Contact.css';
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+    <>
+     <Router>
+    <Header/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+      </Switch>
+      <Footer/>
+    </Router>
+    
+    </>
+  );
+}
+
+export default App;
